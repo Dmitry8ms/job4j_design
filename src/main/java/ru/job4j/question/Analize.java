907ee0095a3a5d952ca6656e954406175e10de08
Dmitry8ms <dshatokhin@yandex.ru>
2021-08-16 15:01:22 +0700

    2. Статистика по коллекции. [#45889 #189942]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,6 +1,5 @@
 package ru.job4j.question;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,24 +7,23 @@ import java.util.stream.Collectors;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        int added = 0;
+        int added;
         int changed = 0;
         int deleted = 0;
-        Map<Integer, String> idNamePrev = previous.stream().collect(
-                Collectors.toMap(u -> u.getId(), u -> u.getName()));
+
         Map<Integer, String> idNameCur = current.stream().collect(
-                Collectors.toMap(u -> u.getId(), u -> u.getName()));
-        for (Map.Entry<Integer, String> entry : idNamePrev.entrySet()) {
-            int id = entry.getKey();
+                Collectors.toMap(User::getId, User::getName));
+        for (User u : previous) {
+            int id = u.getId();
             if (!idNameCur.containsKey(id)) {
                 deleted++;
             } else {
-                if (!idNamePrev.get(id).equals(idNameCur.get(id))) {
+                if (!u.getName().equals(idNameCur.get(id))) {
                     changed++;
                 }
             }
         }
-        added = idNameCur.size() + deleted - idNamePrev.size();
+        added = idNameCur.size() + deleted - previous.size();
         return new Info(added, changed, deleted);
     }
 
