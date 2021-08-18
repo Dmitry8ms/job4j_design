83065175ad37003126f968f060bd333b39e2915d
Dmitry8ms <dshatokhin@yandex.ru>
2021-08-18 20:34:04 +0700

    4.2. Поиск дубликатов [#315066 #191325]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -12,13 +12,11 @@ import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     private Map<FileProperty, Integer> dupCount = new HashMap<>();
-    private int count = 1;
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        dupCount.computeIfPresent(new FileProperty(file.toFile().getName(), file.toFile().length()),
-                (k, v) -> v = v + 1);
-        dupCount.putIfAbsent(new FileProperty(file.toFile().getName(), file.toFile().length()),
-                1);
+        FileProperty fileInfo = new FileProperty(file.toFile().getName(), file.toFile().length());
+        dupCount.computeIfPresent(fileInfo, (k, v) -> v = v + 1);
+        dupCount.putIfAbsent(fileInfo, 1);
         return super.visitFile(file, attrs);
     }
     public List<FileProperty> getDups() {
