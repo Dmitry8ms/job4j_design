6f40f89516a1091526b2c99a26dd254f59a9bfdb
Dmitry8ms <dshatokhin@yandex.ru>
2021-08-20 22:31:30 +0700

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -9,9 +11,18 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public static void packFiles(List<Path> sources, File target) {
-        for (Path source : sources) {
-            packSingleFile(source, target);
+        try (ZipOutputStream zipOut = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zipOut.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source.toString()))) {
+                    //System.out.println("zipping " + source);
+                    zipOut.write(in.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
     public static void packSingleFile(Path source, File target) {
@@ -26,8 +37,17 @@ public class Zip {
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException(
+                    "Folder to archive, file exclusion, or archive name is null. "
+                            + "Usage java -jar pack.jar -d=ROOT_FOLDER -e=EXCLUSION_EXTENSION -o=SOURCE_ARCHIVE_NAME.");
+        }
         ArgsName arguments = ArgsName.of(args);
         Path root = Path.of(arguments.get("d"));
+        if (!Files.exists(root)) {
+            throw new IllegalArgumentException(
+                    "Specified root folder does not exist. Please, specify an existing folder");
+        }
         String extension = arguments.get("e");
         File target = new File(arguments.get("o"));
         List<Path> sources = Search.search(root, path -> !path.toFile().getName().endsWith(extension));
