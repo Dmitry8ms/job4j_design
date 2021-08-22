4a997d7f6deddea8e9b1d83f298b42f1bf337c19
Dmitry8ms <dshatokhin@yandex.ru>
2021-08-22 12:47:55 +0700

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,69 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        List<String> log = new LinkedList<>();
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+            List<String> botAnswers = readPhrases();
+            String phraseFromConsole;
+            String startStop = CONTINUE;
+            String botAnswer;
+            phraseFromConsole = console.readLine();
+            while (!phraseFromConsole.equals(OUT)) {
+                log.add(phraseFromConsole);
+                if (phraseFromConsole.equals(STOP)) {
+                    startStop = STOP;
+                } else if (phraseFromConsole.equals(CONTINUE)) {
+                    startStop = CONTINUE;
+                }
+                if (startStop.equals(CONTINUE)) {
+                    botAnswer = botAnswers.get((int) (Math.random() * botAnswers.size()));
+                    System.out.println(botAnswer);
+                    log.add(botAnswer);
+                }
+                phraseFromConsole = console.readLine();
+            }
+            saveLog(log);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private List<String> readPhrases() throws IOException {
+        return Files.lines(Path.of(botAnswers), StandardCharsets.UTF_8)
+                .collect(Collectors.toList());
+    }
+
+    private void saveLog(List<String> log) throws IOException {
+        Files.write(Path.of(path), log, StandardCharsets.UTF_8);
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("log.txt", "botAnswers.txt");
+        cc.run();
+    }
+}
