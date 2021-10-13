54a57b5b48ab44b915728426c3c99ca969998218
Dmitry8ms <dshatokhin@yandex.ru>
2021-10-13 13:13:22 +0700

    0. Что такое база данных. [#6860]

--- data/create staff.sql
+++ data/create staff.sql
@@ -1,6 +1,6 @@
 create table staff(id serial primary key, firstname varchar(256), surname varchar(256),
 position text, age integer);
-insert into staff(name, surname, position, age) values('Ivan', 'Ivanov', 'Senior assistent of junior assistent', 18);
+insert into staff(firstname, surname, position, age) values('Ivan', 'Ivanov', 'Senior assistent of junior assistent', 18);
 select * from staff;
 update staff set position = 'Senior assistent of junior assistent of junior programmer';
 delete from staff;
