8e742ab307242dee0f99147645e490ddc5f65940
Dmitry8ms <dshatokhin@yandex.ru>
2021-10-18 18:19:59 +0700

    0. Select [#6861]

---
+++ data/fauna.sql
@@ -0,0 +1,21 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+select * from fauna;
+insert into fauna(name, avg_age, discovery_date) values(
+			'Zubatik polosaty', 10001, '1949-10-1');
+insert into fauna(name, avg_age, discovery_date) values(
+			'Zubatik zubaty', 12000, '1949-10-1');
+insert into fauna(name, avg_age, discovery_date) values(
+			'Rusty saw fish', 20000, '1920-10-5');
+insert into fauna(name, avg_age, discovery_date) values(
+			'Fish of fucking glory', 50000, '1900-1-1');
+insert into fauna(name, avg_age, discovery_date) values(
+			'Transgender groundhog', 5000, null);
+select * from fauna where name like '%fish%' or name like '%Fish%';
+select * from fauna where avg_age between 10000 and 21000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < '1950-1-1';
